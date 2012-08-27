d9329b38acf5911de70adb35da982196f60e5d5e
Daisy Zhou <daisy.zdx@gmail.com>
2012-08-27 09:58:43 -0700

    Issue 2. Moved message chat effects to ChatEffects.scala

--- src/main/scala/com/goldblastgames/Message.scala
+++ src/main/scala/com/goldblastgames/Message.scala
@@ -13,34 +13,6 @@ case class Message(
   channel: String,
   body: String
 ) {
-  // Character to represent redactions
-  val redactChar = "█"
-
-  // Redact every redactEvery words
-  val redactEvery = 3
-
-  // Anonymize the message (sender -> "anonymous")
-  def anonymize: Message = {
-    return new Message("anonymous", channel, body)
-  }
-
-  // Redact parts of the message randomly
-  def redact: Message = {
-    val random = new Random((sender + body).hashCode)
-    val redacted =
-      body.split("""\s+""") // Split on whitespace
-        .map(word => 
-          random.nextInt(redactEvery) match {
-            // If random int equals 0, redact it.
-            case 0 => redactChar * word.length
-            case _ => word
-          }
-        ).mkString(" ") // Put it back together into a string.
-      // TODO You lose tabs/other whitespace when this happens.
-
-    return new Message(sender, channel, redacted)
-  }
-
   override def toString(): String = {
     return "%s: %s: %s".format(channel, sender, body)
   }
